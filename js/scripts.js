7347f7220e3fefeeed4ff6ab9e7f88fd2a72827f
alexbensusan <alexrbensusan@gmail.com>
2024-02-12 13:50:39 -0800

    added hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("hi, welcome to my site")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
